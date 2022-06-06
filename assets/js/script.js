d8fcdbf1d996a069905708b84cc3ef5f79b82185
@henrique.marques23 <luiz@isket.com.br>
2022-06-05 23:30:51 -0300

    adicionado animacao e corrigindo espaçamentos

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,30 @@ accordionItemEl.forEach(acc => {
     acc.classList.toggle('open');
   });
 });
+
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true,
+});
+
+ScrollReveal().reveal('#hero .subtitle, #hero .title, #hero .button, #hero .image', {
+  interval: 100,
+});
+ScrollReveal().reveal('#partners .subtitle, #partners .title', {
+  interval: 100,
+});
+ScrollReveal().reveal('#about .subtitle, #about .title, #about .paragraph,  #about .image', {
+  interval: 100,
+});
+ScrollReveal().reveal(
+  '#forWhom .subtitle, #forWhom .title, #forWhom .paragraph,  #forWhom .image',
+  { interval: 100 }
+);
+ScrollReveal().reveal('#points .subtitle, #points .title, #points .paragraph,  #points .image', {
+  interval: 100,
+});
+
+
+// ScrollReveal().reveal('.image, .paragraph', { origin: 'bottom', delay: 300 });
